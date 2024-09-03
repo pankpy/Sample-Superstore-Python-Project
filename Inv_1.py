174adbda2b03aaf81629e2b863a0a88263ca7953
pankpy <pankajs.negi@outlook.com>
2024-09-03 15:02:22 +0530

    Added loop code

--- Inv_1.py
+++ Inv_1.py
@@ -12,7 +12,7 @@ df.drop_duplicates(inplace=True)
 
 
 #Keeping only these columns
-df = df[['Order ID', 'Order Date', 'Ship Date', 
+df = df[['Order ID', 'Order Date', 'Ship Date', 'Region',
        'Customer ID', 'Customer Name', 'Segment', 'Country', 'City', 'State',
        'Postal Code','Product ID',
        'Product Name', 'Sales', 'Quantity', 'Discount', 'Profit']]
@@ -21,6 +21,10 @@ df = df[['Order ID', 'Order Date', 'Ship Date',
 # Order ID wise
 
 print(df.columns)
+
+
+# df = df['Region']
+df = df[df['Region'] == 'West']
 
 # order_id_list0 = df['Order ID'].tolist()
 # print(len(order_id_list0))
@@ -31,6 +35,12 @@ print(len(order_id_list))
 
 #Note: There are 5009 distinct Order ID. It would create 5009 invoices in Invoices folder and would take a lot of space. 
 #For demonstration, I am adding a Region filter
+
+for ord in order_id_list:
+    df1 = df[df['Order ID'] == ord]
+
+
+    print('DF1', df1)
 
 
 
