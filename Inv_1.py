6e39d1d58417be17771bf69ff00f858c20823abe
pankpy <pankajs.negi@outlook.com>
2024-09-12 15:06:02 +0530

    saved changes

--- Inv_1.py
+++ Inv_1.py
@@ -20,39 +20,66 @@ df = df[['Order ID', 'Order Date', 'Ship Date', 'Region',
 # Group Data 
 # Order ID wise
 
-print(df.columns)
-
-
-# df = df['Region']
-df = df[df['Region'] == 'West']
-
-# order_id_list0 = df['Order ID'].tolist()
-# print(len(order_id_list0))
-
-#Creating list of distinct  Order ID
-order_id_list = df['Order ID'].unique().tolist()
-print(len(order_id_list))
-
-#Note: There are 5009 distinct Order ID. It would create 5009 invoices in Invoices folder and would take a lot of space. 
-#For demonstration, I am adding a Region filter
-
-
-
-for ord in order_id_list:
-    df1 = df[df['Order ID'] == ord]
-
-
-#     print('DF1', df1)
 
 #Accessing invoice template and pasting data from df1 to invoice template.
 inv_temp_wb = openpyxl.load_workbook("Source/invoice template.xlsx")
+ws = inv_temp_wb["Invoice"]
 
 # inv_temp_wb.save('C:\\Users\\panka\\OneDrive\\Desktop\\Abcd.xlsx')
 
 # Iterating column and row wise in invoice template
+# function to find cell next to Or below required fiels in invoice.
+def cell_for_entering_value_finder(inv_field):
+    for i in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
+        for j in i:
+            # print("Sheet",j.value)
+            if j.value == inv_field:
+                if inv_field in ["Product ID","Product Name","Unit Price","Quantity","Price"]:
+                # Cell_num will be the cell in which value of inv field will be enter for a customer.
+                    cell_num = ws.cell(row=j.row+1, column=j.column)
+                else:
+                    cell_num = ws.cell(row=j.row, column=j.column+1)
 
-for i in inv_temp_wb:
-    print(i)
+    return cell_num
+
+inv_field = "Customer Name"
+a = cell_for_entering_value_finder(inv_field)
+print(a)
+
+# fields for invoice from df1
+
+print(df.columns)
+
+#Creating list of distinct  Order ID
+order_id_list = df['Order ID'].unique().tolist()
+# print(len(order_id_list))
+
+#Note: There are 5009 distinct Order ID. It would create 5009 invoices in Invoices folder and would take a lot of space. 
+#For demonstration, I am adding a Region filter
+df = df[df['Region'] == 'West']
 
 
 
+# def fill_invoice(df1):
+#     breakpoint()    
+#     custID = (df1['Customer ID'].unique.tolist())[0]
+#     custname = (df1['Customer Name'].unique.tolist())[0]
+#     segm = (df1['Segment'].unique.tolist())[0]
+#     df1['address'] = df1['City'] + df1['State'] + df1['Country']
+#     address = (df1['address'].unique.tolist())[0]
+#     oid = (df1['Order ID'].unique.tolist())[0]
+#     odt = (df1['Order Date'].unique.tolist())[0]
+#     sdt = (df1['Ship Date'].unique.tolist())[0]
+#     ord_details = (df1['Order Details'].unique.tolist())[0]
+#     prod_ID = (df1['Product ID'].unique.tolist())[0]
+#     prod_name = (df1['Product Name'].unique.tolist())[0]
+#     unit_price = (df1['Unit Price'].unique.tolist())[0]
+#     qty = (df1['Quantity'].unique.tolist())[0]
+
+#     print(custID, custname, segm, address, oid, odt, sdt, ord_details, prod_ID, prod_name, unit_price, qty)
+
+for ord in order_id_list:
+    df1 = df[df['Order ID'] == ord]
+    # print(df1)
+#   print('DF1', df1)
+    # fill_invoice(df1)
--- Inv_3Rough.py
+++ Inv_3Rough.py
@@ -3,125 +3,25 @@ import openpyxl
 from openpyxl import cell
 
 inv_temp_wb = openpyxl.load_workbook("Source/invoice template.xlsx")
+ws = inv_temp_wb["Invoice"]
 
 # inv_temp_wb.save('C:\\Users\\panka\\OneDrive\\Desktop\\Abcd.xlsx')
 
-# Iterating column and row wise in invoice template
+# Iterating column and row wise in invoice template to find cell next or below fields, so that values can be enter in them.
 
-ws = inv_temp_wb["Invoice"]
+def cell_for_entering_value_finder(inv_field):
+    for i in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
+        for j in i:
+            # print("Sheet",j.value)
+            if j.value == inv_field:
+                if inv_field in ["Product ID","Product Name","Unit Price","Quantity","Price"]:
+                # Cell_num will be the cell in which value of inv field will be enter for a customer.
+                    cell_num = ws.cell(row=j.row+1, column=j.column)
+                else:
+                    cell_num = ws.cell(row=j.row, column=j.column+1)
 
-for i in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
-    for j in i:
-        # print("Sheet",j.value)
-        if j.value == "Customer ID":
-            # Cell D9 will be the cell in which value of Customer ID will be enter for a customer.
-            D9 = ws.cell(row=j.row, column=j.column+1)
-            print(D9)            
-            # print(j)
+    return cell_num
 
-for i in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
-    for j in i:
-        if j.value == "Customer Name":
-            # Cell D10 will be the cell in which value of Customer Name will be enter for a customer.
-            D10 = ws.cell(row=j.row, column=j.column+1)
-            print(D10)            
-            # print(j)
-
-for i in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
-    for j in i:
-        if j.value == "Segment":
-            # Cell D11 will be the cell in which value of Segment will be enter for a customer.
-            D11 = ws.cell(row=j.row, column=j.column+1)
-            print(D11)            
-            # print(j)
-
-for i in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
-    for j in i:
-        if j.value == "Address":
-            # Cell D12 will be the cell in Address will be enter for a customer.
-            D12 = ws.cell(row=j.row, column=j.column+1)
-            print(D12)            
-            # print(j)
-
-for i in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
-    for j in i:
-        if j.value == "Order Details":
-            # Cell D15 will be the cell in which Order Details will be enter for a customer.
-            D15 = ws.cell(row=j.row, column=j.column+1)
-            print(D15)            
-            # print(j)
-
-
-for i in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
-    for j in i:
-        if j.value == "Order ID":
-            # Cell M9 will be the cell in which Order ID will be enter for a customer.
-            M9 = ws.cell(row=j.row, column=j.column+1)
-            print(M9)            
-            # print(j)
-
-
-for i in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
-    for j in i:
-        if j.value == "Order Date":
-            # Cell M10 will be the cell in which Order Date will be enter for a customer.
-            M10 = ws.cell(row=j.row, column=j.column+1)
-            print(M10)            
-            # print(j)
-
-
-for i in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
-    for j in i:
-        if j.value == "Ship Date":
-            # Cell M11 will be the cell in which Ship Date will be enter for a customer.
-            M11 = ws.cell(row=j.row, column=j.column+1)
-            print(M11)            
-            # print(j)
-
-
-for i in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
-    for j in i:
-        if j.value == "Product ID":
-            # Cell C18 will be the cell in which Product ID will be enter for a customer.
-            C18 = ws.cell(row=j.row+1, column=j.column)
-            print(C18)            
-            # print(j)
-
-for i in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
-    for j in i:
-        if j.value == "Product Name":
-            # Cell D18 will be the cell in which Product Name will be enter for a customer.
-            D18 = ws.cell(row=j.row+1, column=j.column)
-            print(D18)            
-            # print(j)
-
-for i in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
-    for j in i:
-        if j.value == "Unit Price":
-            # Cell E18 will be the cell in which Unit Price will be enter for a customer.
-            E18 = ws.cell(row=j.row+1, column=j.column)
-            print(E18)            
-            # print(j)
-
-for i in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
-    for j in i:
-        if j.value == "Quantity":
-            # Cell F18 will be the cell in which Quantity will be enter for a customer.
-            F18 = ws.cell(row=j.row+1, column=j.column)
-            print(F18)            
-            # print(j)
-
-for i in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
-    for j in i:
-        if j.value == "Price":
-            # Cell F18 will be the cell in which Price will be enter for a customer.
-            F18 = ws.cell(row=j.row+1, column=j.column)
-            print(F18)            
-            # print(j)
-
-
-# I will be converting this into a function in Inv_3Rough.py
-
-
-
-
+inv_field = "Customer Name"
+a = cell_for_entering_value_finder(inv_field)
+print(a)
